8b59028ed48a6e23da2970b4f0f9211f17404b42
marcospmoliveira <marcospaulomeiradeoliveira@gmail.com>
2019-02-01 20:26:39 -0200

    01/02/2019

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,25 @@
 module.exports = function(app){	
 	
 	app.get('/formulario_inclusao_noticia', function(req, res){
-		res.render("admin/form_add_noticia");
+		res.render("admin/form_add_noticia", {validacao : {},noticia : {} });
 	});
 
 	app.post('/noticias/salvar', function(req, res){
 		var noticia = req.body;
+
+		req.assert('titulo','Título é obrigatório').notEmpty();
+		req.assert('resumo','Resumo é obrigatório').notEmpty();
+		req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+		req.assert('autor','Autor é obrigatório').notEmpty();
+		req.assert('data_noticia','Data é obrigatória').notEmpty();
+		req.assert('noticia','Noticia é obrigatória').notEmpty();
+
+		var erros = req.validationErrors();
+
+		if(erros){
+			res.render("admin/form_add_noticia", {validacao : erros, noticia : noticia});
+			return;
+		}
 		
 		var connection = app.config.dbConnection();
 		var noticiasModel = new app.app.models.NoticiasDAO(connection);

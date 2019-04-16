652ab59d4e91080952d623332db2f6e7553d23fc
marcospmoliveira <marcospaulomeiradeoliveira@gmail.com>
2019-04-16 20:33:35 -0300

    16/04/2019

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -11,6 +11,8 @@ module.exports.noticia = function(app, req, res){
 	var connection = app.config.dbConnection();
 	var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
+	var id_noticias = req.query;
+
 	noticiasModel.getNoticia(function(error, result){
 		res.render("noticias/noticia", {noticia : result});
 	});	
